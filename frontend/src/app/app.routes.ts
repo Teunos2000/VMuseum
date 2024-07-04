905ed0de675601aaf06871ab9786319072043ff4
Teunos2000 <teun.hilbers2000@gmail.com>
2024-07-04 15:32:39 +0200

    navbar active fixed, routes fixed and login and home component created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
-import { NavbarComponent} from "./components/navbar/navbar.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginComponent } from "./components/login/login.component";
+import { HomeComponent } from "./components/home/home.component";
 
 export const routes: Routes = [
-  { path: 'first-component', component: NavbarComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // default route to home
 ];
