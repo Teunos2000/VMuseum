c8d16e2c6c57e40b026b48b7d6df546aed4d1220
Teunos2000 <teun.hilbers2000@gmail.com>
2024-07-10 00:45:57 +0200

    profile component made

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from "./components/home/home.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // default route to home
 ];
